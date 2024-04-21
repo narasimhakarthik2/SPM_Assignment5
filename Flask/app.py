e36d56e2a27a0fe8e8b1ebda68635e2a37045524
Narasimha Karthik <narasimhakarthik3@gmail.com>
2024-04-21 16:21:26 -0500

    updates

--- Flask/app.py
+++ Flask/app.py
@@ -121,7 +121,7 @@ def github():
     repo_name = body['repository']
     # Add your own GitHub Token to run it local
     token = os.environ.get(
-        'GITHUB_TOKEN', YOUR_GITHUB_TOKEN)
+        'GITHUB_TOKEN','')
     GITHUB_URL = f"https://api.github.com/"
     headers = {
         "Authorization": f'token {token}'
